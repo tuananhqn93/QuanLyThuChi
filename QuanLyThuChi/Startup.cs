c4c8dd7ec221fa9ec077462b43aa4d28f998c02c
anhpt <tuananhqn93@gmail.com>
2015-08-10 12:50:23 +0700

    init

---
+++ QuanLyThuChi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyThuChi.Startup))]
+namespace QuanLyThuChi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
